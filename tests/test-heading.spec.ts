90ab8ced00df1f6e139b76fd4d998f8ad064f161
Guy Stals <guy_stals@peopleware.be>
2026-09-22 10:03:01 +0200

    remove playwright tests

--- tests/test-heading.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Portfolio Guy Stals' })).toBeVisible();
-});
--- tests/test-projects-checkboxes.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://guystals.github.io/Portfolio/');
-  await page.getByRole('link', { name: 'Projects', exact: true }).click();
-  await page.getByRole('button', { name: 'Desktop' }).click();
-  await page.getByRole('button', { name: 'Integration' }).click();
-  await page.getByRole('button', { name: 'Web' }).click();
-});
